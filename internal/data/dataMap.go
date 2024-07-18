b8e1e7aa29d655bf5af3bdefd5417e527221ee89
afauble <andrew.fauble@gmail.com>
2024-07-18 14:36:00 -0400

    Filled rest of dataMap

--- internal/data/dataMap.go
+++ internal/data/dataMap.go
@@ -13,22 +13,22 @@ var (
 	cannonHealth           [22]float32
 	archerTowerHealth      [22]float32
 	mortarHealth           [17]float32
-	airDefenseHealth       []float32
-	wizardTowerHealth      []float32
-	airSweeperHealth       []float32
-	hiddenTeslaHealth      []float32
-	bombTowerHealth        []float32
-	xBowHealth             []float32
-	infernoTowerHealth     []float32
-	eagleArtilleryHealth   []float32
-	scattershotHealth      []float32
-	builderHutHealth       []float32
-	spellTowerHealth       []float32
-	monolithHealth         []float32
-	multiArcherTowerHealth []float32
-	ricochetCannonHealth   []float32
-	townHallHealth         []float32
-	clanCastleHealth       []float32
+	airDefenseHealth       [15]float32
+	wizardTowerHealth      [17]float32
+	airSweeperHealth       [8]float32
+	hiddenTeslaHealth      [16]float32
+	bombTowerHealth        [12]float32
+	xBowHealth             [12]float32
+	infernoTowerHealth     [11]float32
+	eagleArtilleryHealth   [8]float32
+	scattershotHealth      [6]float32
+	builderHutHealth       [7]float32
+	spellTowerHealth       [4]float32
+	monolithHealth         [4]float32
+	multiArcherTowerHealth [3]float32
+	ricochetCannonHealth   [3]float32
+	townHallHealth         [17]float32
+	clanCastleHealth       [13]float32
 
 	buildingNameMap map[string][]float32
 )
@@ -44,22 +44,22 @@ func init() {
 	cannonHealth = [22]float32{0, 420, 470, 520, 570, 620, 670, 730, 800, 880, 960, 1060, 1160, 1260, 1380, 1500, 1620, 1740, 1870, 2000, 2150, 2250}
 	archerTowerHealth = [22]float32{0, 380, 420, 460, 500, 540, 580, 630, 690, 750, 810, 890, 970, 1050, 1130, 1230, 1310, 1390, 1510, 1600, 1700, 1800}
 	mortarHealth = [17]float32{0, 400, 450, 500, 550, 600, 650, 700, 800, 950, 1100, 1300, 1500, 1700, 1950, 2150, 2300}
-	airDefenseHealth = []float32{}
-	wizardTowerHealth = []float32{}
-	airSweeperHealth = []float32{}
-	hiddenTeslaHealth = []float32{}
-	bombTowerHealth = []float32{}
-	xBowHealth = []float32{}
-	infernoTowerHealth = []float32{}
-	eagleArtilleryHealth = []float32{}
-	scattershotHealth = []float32{}
-	builderHutHealth = []float32{}
-	spellTowerHealth = []float32{}
-	monolithHealth = []float32{}
-	multiArcherTowerHealth = []float32{}
-	ricochetCannonHealth = []float32{}
-	townHallHealth = []float32{}
-	clanCastleHealth = []float32{}
+	airDefenseHealth = [15]float32{0, 800, 850, 900, 950, 1000, 1050, 1100, 1210, 1300, 1400, 1500, 1650, 1750, 1850}
+	wizardTowerHealth = [17]float32{0, 620, 650, 680, 730, 840, 960, 1200, 1440, 1600, 1900, 2120, 2240, 2500, 2800, 3000, 3150}
+	airSweeperHealth = [8]float32{0, 750, 800, 850, 900, 950, 1000, 1050}
+	hiddenTeslaHealth = [16]float32{0, 600, 630, 660, 690, 730, 770, 810, 850, 900, 980, 1100, 1200, 1350, 1450, 1550}
+	bombTowerHealth = [12]float32{0, 650, 700, 750, 850, 1050, 1300, 1600, 1900, 2300, 2500, 2700}
+	xBowHealth = [12]float32{0, 1500, 1900, 2300, 2700, 3100, 3400, 3700, 4000, 4200, 4400, 4600}
+	infernoTowerHealth = [11]float32{0, 1500, 1800, 2100, 2400, 2700, 3000, 3300, 3700, 4000, 4400}
+	eagleArtilleryHealth = [8]float32{0, 4000, 4400, 4800, 5200, 5600, 5900, 6200}
+	scattershotHealth = [6]float32{0, 3600, 4200, 4800, 5100, 5410}
+	builderHutHealth = [7]float32{0, 250, 1000, 1300, 1600, 1800, 1900}
+	spellTowerHealth = [4]float32{0, 2500, 2800, 3100}
+	monolithHealth = [4]float32{0, 4747, 5050, 5353}
+	multiArcherTowerHealth = [3]float32{0, 5000, 5200}
+	ricochetCannonHealth = [3]float32{0, 5400, 5700}
+	townHallHealth = [17]float32{0, 450, 1600, 1850, 2100, 2400, 2800, 3300, 3900, 4600, 5500, 6800, 7500, 8200, 8900, 9600, 10000}
+	clanCastleHealth = [13]float32{0, 1000, 1400, 2000, 2600, 3000, 3400, 4000, 4400, 4800, 5200, 5400, 5600}
 
 	// Get Func Map
 	buildingNameMap = map[string][]float32{
