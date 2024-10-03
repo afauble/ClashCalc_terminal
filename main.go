6295a89fcd6fd0194e7c3150afb157834fb239e4
afauble <andrew.fauble@gmail.com>
2024-10-03 16:23:53 -0400

    removed commented code

--- main.go
+++ main.go
@@ -4,18 +4,4 @@ import "github.com/afauble/ClashCalc/internal/frontend"
 
 func main() {
 	frontend.CreateTerminalUI()
-
-	// var num float32 = 1 * 2
-	// var num1 float32 = 1 + num
-	// var num2 float32 = 1 / num1
-	// temp := float32(1 / (1 + (1 * 2)))
-	// fmt.Print(strconv.FormatFloat(float64(num2), 'f', -1, 64))
-
-	// results := helpers.FindOptimalSpells(5410, models.UserInput{
-	// 	GiantarrowLvl: 0,
-	// 	FireballLvl:   0,
-	// 	LightningLvl:  11,
-	// 	EarthquakeLvl: 5,
-	// })
-	// fmt.Print(results.ToString())
 }
